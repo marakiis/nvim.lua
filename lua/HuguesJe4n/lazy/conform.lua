78f8f298887223d9584fc5d64e5b6d4ae64bdfb5
mro <maxime.waarp.org>
2025-07-30 15:13:01 +0200

    add conform for auto format

---
+++ lua/HuguesJe4n/lazy/conform.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmt = { "ConformInfo" },
+
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				go = { "gofumpt" },
+			},
+			default_format_opts = {
+				lsp_format = "fallback",
+			},
+			format_on_save = { timeout_ms = 500 },
+		},
+	},
+}
